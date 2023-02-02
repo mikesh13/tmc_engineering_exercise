6bd3a46eb352ee6f368e48cb5699af1c2270a045
mikesh13 <mcshieh1014@gmail.com>
2023-02-02 03:15:31 -0800

    push new changes

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from download_data import download_data
+from match_voters import match_voters_with_voter_id
 
+if __name__ == '__main__':
+    for i in range(1, 5):
+        download_data(i)
+    
+    match_voters_with_voter_id()
+    print("Done matching. Saved results to matched_data folder.")
--- match_voters.py
+++ match_voters.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 
@@ -19,11 +20,33 @@ def extract_middle_name(txt: str) -> str:
 
 def add_name_col(df: pd.DataFrame) -> pd.DataFrame:
     df['FIRST_NAME'] = df['FIRST_NAME'].transform(lambda x: transform_text(x))
-    df['MIDDLE_NAME'] = df['MIDDLE_NAME'].transform(lambda x: transform_text(x))
+    # df['MIDDLE_NAME'] = df['MIDDLE_NAME'].transform(lambda x: transform_text(x))
     df['LAST_NAME'] = df['LAST_NAME'].transform(lambda x: transform_text(x))
     df['name'] = df[['FIRST_NAME', 'LAST_NAME']].agg(' '.join, axis=1)
 
-    return df[['matched_voter_id', 'name', 'MIDDLE_NAME']]
+    return df
+
+
+def extract_year(birth_date: str) -> str:
+    if birth_date == np.nan or birth_date == '':
+        return birth_date
+    else:
+        year = birth_date.split('-')[0]
+        return year
+
+
+def transform_df(df: pd.DataFrame) -> pd.DataFrame:
+    df = add_name_col(df)
+    df['birth_year'] = (df.birth_year.apply(lambda x: extract_year(x)))
+
+    df = (df[['matched_voter_id', 'name', 'birth_year', 'zip']]
+          .copy()
+          .astype({
+            'name': object,
+            'zip': str
+            }))
+
+    return df
 
 
 def merge_county_dfs():
@@ -32,31 +55,32 @@ def merge_county_dfs():
 
     df = pd.concat(map(pd.read_csv, data_list), ignore_index=True)
     df = (
-        df[['SOS_VOTERID', 'FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME']]
+        df[['SOS_VOTERID', 'FIRST_NAME', 'LAST_NAME',
+            'DATE_OF_BIRTH', 'RESIDENTIAL_ZIP']]
         .dropna()
         .copy()
-        .rename(columns={'SOS_VOTERID': 'matched_voter_id'})
+        .rename(columns={
+            'SOS_VOTERID': 'matched_voter_id',
+            'DATE_OF_BIRTH': 'birth_year',
+            'RESIDENTIAL_ZIP': 'zip'
+            })
         )
 
-    df = add_name_col(df)
+    df = transform_df(df)
 
     return df
 
 
 def match_voters_with_voter_id():
     county_df = merge_county_dfs()
-    voter_df = pd.read_csv('matching_data/eng-matching-input-v3.csv')
-    voter_df['MIDDLE_NAME'] = voter_df.name.apply(
-        lambda x: extract_middle_name(x))
-    print(voter_df.head())
+    voter_df = pd.read_csv('matching_data/eng-matching-input-v3.csv').dropna()
 
-    #merged_df = pd.merge(county_df, voter_df, on=['name', 'MIDDLE_NAME'], how='inner')
-    merged_df = pd.merge(county_df, voter_df, on='name', how='inner')
-    #final_cols = merged_df.columns.remove('MIDDLE_NAME')
-    #merged_df[final_cols].to_csv('matched_data/matched_data.csv', index=False)
+    voter_df['birth_year'] = voter_df.birth_year.apply(lambda x: str(int(x)))
+    voter_df['zip'] = voter_df.zip.apply(lambda x: str(x)[:-2])
+
+    merged_df = pd.merge(county_df, voter_df, on=['name', 'birth_year', 'zip'],
+                         how='inner')
     merged_df.to_csv('matched_data/matched_data.csv', index=False)
 
 
-#print(match_voters_with_voter_id())
-#print(merge_county_dfs().head())
 match_voters_with_voter_id()
